25ebb1fa62ed86ef5dc1633f57d0c1bb81fc3154
Will Wong <williamwj.wong@gmail.com>
2022-02-28 16:12:11 -0800

    Implement Lambda component model and props

--- lib/nft_collection_serverless-stack.ts
+++ lib/nft_collection_serverless-stack.ts
@@ -2,6 +2,12 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+
+export type ConstructWithAppScope = Construct & {
+  account: string;
+  region: string;
+}
+
 export class NftCollectionServerlessStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
